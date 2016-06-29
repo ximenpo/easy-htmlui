7231c89a330185c42816ff4c62edde380263c88f
ximenpo <ximenpo@jiandan.ren>
2016-06-29 10:01:32 +0800

    FIX external object release bug.

--- WebCustomizer.cpp
+++ WebCustomizer.cpp
@@ -6,11 +6,13 @@
 WebCustomizer::WebCustomizer(void)
 	:	m_pExternal(new WebExternalObject())
 {
+	m_pExternal->AddRef();
 	this->SetExternalDispatch(m_pExternal);
 }
 
 
 WebCustomizer::~WebCustomizer(void)
 {
+	this->SetExternalDispatch(NULL);
 	delete	m_pExternal;
 }
